dfc2c8b702af9b2f57ada4b33edacdf7e2406490
Md. Tareq Mahmood <tareq.py@gmail.com>
2025-12-02 11:26:26 -0600

    Move to new site

--- podman.sh
+++
@@ -1,6 +0,0 @@
-podman run --rm -it \
-  -p 4000:4000 \
-  -v "$PWD:/srv/jekyll:z" \
-  -v "$PWD/vendor/bundle:/usr/local/bundle:z" \
-  docker.io/jekyll/jekyll:latest \
-  jekyll serve --watch --host 0.0.0.0
